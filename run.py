07346ebeb0b47484078c2ae1c2af8dbc2edc0c9d
Jacob <Jacobshortall@gmail.com>
2021-07-29 21:41:15 +0100

    Create class to hold post information & add method to get titles from page

--- run.py
+++ run.py
@@ -6,3 +6,12 @@ newest_response = requests.get("https://news.ycombinator.com/newest").text
 
 trending_html = BeautifulSoup(trending_response, "html.parser")
 newest_html = BeautifulSoup(newest_response, "html.parser")
+
+
+class Posts:
+    def __init__(self, html):
+        self.html = html
+
+    def get_titles(self):
+        html_titles = self.html.find_all(class_="storylink")
+        return [title.text for title in html_titles]
