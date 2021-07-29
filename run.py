2705225534085c5d8471dfb2292e8dd17bec7518
Jacob <Jacobshortall@gmail.com>
2021-07-29 21:38:19 +0100

    Request URLs and instantiate BeautifulSoup objects from responses

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-# python code goes here
+import requests
+from bs4 import BeautifulSoup
+
+trending_response = requests.get("https://news.ycombinator.com/news").text
+newest_response = requests.get("https://news.ycombinator.com/newest").text
+
+trending_html = BeautifulSoup(trending_response, "html.parser")
+newest_html = BeautifulSoup(newest_response, "html.parser")
