5a2cbdb33786d7a44f3c1fc02526674a3b27b212
Jacob <Jacobshortall@gmail.com>
2021-08-19 18:24:32 +0100

    Move functions to bottom of run.py

--- run.py
+++ run.py
@@ -6,6 +6,72 @@ TRENDING_URL = "https://news.ycombinator.com/news"
 NEWEST_URL = "https://news.ycombinator.com/newest"
 
 
+class Posts:
+    def __init__(self, url):
+        self.url = url
+
+    def get_info(self):
+        """Fetch post data from given HTML and return a list containing a
+        dictionary for each post (30 posts total)."""
+
+        response = requests.get(self.url)
+        html = BeautifulSoup(response.text, "html.parser")
+
+        html_titles = html.find_all(class_="storylink")
+        html_ages = html.find_all(class_="age")
+
+        li = []
+        for i in range(30):
+            di = {}
+            di["title"] = html_titles[i].text
+            di["link"] = html_titles[i]["href"]
+            di["age"] = html_ages[i].text
+            li.append(di)
+        return li
+
+
+class Printer():
+    def __init__(self, posts):
+        self.posts = posts
+
+    def request_more(self, available_posts):
+        """Ask user if they want to view more posts and, if so, how many."""
+
+        user_input = validate_choice(
+            "Do you want to see more posts? (yes/no):\n", "yes", "no")
+        if user_input == "no":
+            return False
+        return get_count(available_posts)
+
+    def print_info(self, count):
+        """Print initially requested post information to the terminal and ask
+        the user if they want to view more posts using request_more function."""
+
+        shown_posts = 0
+        to_show = count
+
+        while True:
+            for i in self.posts[shown_posts: shown_posts + to_show]:
+                print(
+                    f"""
+                    {self.posts.index(i) + 1}) Title: {i["title"]}
+                    Link: {i["link"]}
+                    Posted: {i["age"]}
+                    """
+                )
+
+            shown_posts += to_show
+            if shown_posts == 30:
+                print("No more posts to show.")
+                break
+
+            # Asks user if they want to see more items
+            user_ans = self.request_more(30 - shown_posts)
+            if not user_ans:
+                break
+            to_show = user_ans
+
+
 def get_count(available_posts):
     """Ask user how many posts they would like to view. Pass input to
     validate_count() and return count once validated. If number is 0, ask user if they want to exit program."""
@@ -71,72 +137,6 @@ def view_posts(post_choice):
 
     printer = instantiate_class(post_choice)
     return printer.print_info(get_count(30))
-
-
-class Posts:
-    def __init__(self, url):
-        self.url = url
-
-    def get_info(self):
-        """Fetch post data from given HTML and return a list containing a
-        dictionary for each post (30 posts total)."""
-
-        response = requests.get(self.url)
-        html = BeautifulSoup(response.text, "html.parser")
-
-        html_titles = html.find_all(class_="storylink")
-        html_ages = html.find_all(class_="age")
-
-        li = []
-        for i in range(30):
-            di = {}
-            di["title"] = html_titles[i].text
-            di["link"] = html_titles[i]["href"]
-            di["age"] = html_ages[i].text
-            li.append(di)
-        return li
-
-
-class Printer():
-    def __init__(self, posts):
-        self.posts = posts
-
-    def request_more(self, available_posts):
-        """Ask user if they want to view more posts and, if so, how many."""
-
-        user_input = validate_choice(
-            "Do you want to see more posts? (yes/no):\n", "yes", "no")
-        if user_input == "no":
-            return False
-        return get_count(available_posts)
-
-    def print_info(self, count):
-        """Print initially requested post information to the terminal and ask
-        the user if they want to view more posts using request_more function."""
-
-        shown_posts = 0
-        to_show = count
-
-        while True:
-            for i in self.posts[shown_posts: shown_posts + to_show]:
-                print(
-                    f"""
-                    {self.posts.index(i) + 1}) Title: {i["title"]}
-                    Link: {i["link"]}
-                    Posted: {i["age"]}
-                    """
-                )
-
-            shown_posts += to_show
-            if shown_posts == 30:
-                print("No more posts to show.")
-                break
-
-            # Asks user if they want to see more items
-            user_ans = self.request_more(30 - shown_posts)
-            if not user_ans:
-                break
-            to_show = user_ans
 
 
 def main():
